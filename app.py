c993fc2e2c89810a057733fa8e8986040bd0421d
James Li <james098093@gmail.com>
2024-12-14 22:48:51 +0800

    Built a search bar

--- app.py
+++ app.py
@@ -15,3 +15,18 @@ Session(app)
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///manager.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+#Things above are from CS50
+
+@app.route("/")
+def index():
+    return render_template("index.html")
--- manager.sql
+++ manager.sql
@@ -8,3 +8,18 @@ CREATE TABLE users (
 CREATE TABLE sqlite_sequence(name,seq);
 
 CREATE UNIQUE INDEX username ON users (username);
+
+-- Information of the books
+CREATE TABLE books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    isbn TEXT NOT NULL,
+    author TEXT NOT NULL,
+    title TEXT NOT NULL,
+    publish_area TEXT NOT NULL,
+    publisher TEXT NOT NULL,
+    year INTEGER NOT NULL,
+    translator TEXT,
+    translated_from_isbn TEXT,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
